baaebc2fe18cf04bde8de7906f884d9bf9d18184
Anthony Ogundipe <diltony@gmail.com>
2024-06-16 02:29:09 +0100

    clean-up for promoted migration

--- api/migrations/Version20240616004140.php
+++ api/migrations/Version20240616004140.php
@@ -19,30 +19,6 @@ final class Version20240616004140 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE book ALTER id TYPE UUID');
-        $this->addSql('COMMENT ON COLUMN book.id IS \'\'');
-        $this->addSql('ALTER TABLE bookmark ALTER id TYPE UUID');
-        $this->addSql('ALTER TABLE bookmark ALTER user_id TYPE UUID');
-        $this->addSql('ALTER TABLE bookmark ALTER book_id TYPE UUID');
-        $this->addSql('ALTER TABLE bookmark ALTER bookmarked_at TYPE TIMESTAMP(0) WITHOUT TIME ZONE');
-        $this->addSql('COMMENT ON COLUMN bookmark.id IS \'\'');
-        $this->addSql('COMMENT ON COLUMN bookmark.user_id IS \'\'');
-        $this->addSql('COMMENT ON COLUMN bookmark.book_id IS \'\'');
-        $this->addSql('COMMENT ON COLUMN bookmark.bookmarked_at IS \'\'');
-        $this->addSql('ALTER TABLE parchment ALTER id TYPE UUID');
-        $this->addSql('COMMENT ON COLUMN parchment.id IS \'\'');
-        $this->addSql('ALTER TABLE review ALTER id TYPE UUID');
-        $this->addSql('ALTER TABLE review ALTER user_id TYPE UUID');
-        $this->addSql('ALTER TABLE review ALTER book_id TYPE UUID');
-        $this->addSql('ALTER TABLE review ALTER published_at TYPE TIMESTAMP(0) WITHOUT TIME ZONE');
-        $this->addSql('COMMENT ON COLUMN review.id IS \'\'');
-        $this->addSql('COMMENT ON COLUMN review.user_id IS \'\'');
-        $this->addSql('COMMENT ON COLUMN review.book_id IS \'\'');
-        $this->addSql('COMMENT ON COLUMN review.published_at IS \'\'');
-        $this->addSql('ALTER TABLE "user" ALTER id TYPE UUID');
-        $this->addSql('COMMENT ON COLUMN "user".id IS \'\'');
-
         // Add isPromoted column
         $this->addSql('ALTER TABLE book ADD is_promoted BOOLEAN DEFAULT FALSE NOT NULL');
         // Set default value for existing records
@@ -51,31 +27,6 @@ final class Version20240616004140 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE parchment ALTER id TYPE UUID');
-        $this->addSql('COMMENT ON COLUMN parchment.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('ALTER TABLE book ALTER id TYPE UUID');
-        $this->addSql('COMMENT ON COLUMN book.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('ALTER TABLE review ALTER id TYPE UUID');
-        $this->addSql('ALTER TABLE review ALTER published_at TYPE TIMESTAMP(0) WITHOUT TIME ZONE');
-        $this->addSql('ALTER TABLE review ALTER user_id TYPE UUID');
-        $this->addSql('ALTER TABLE review ALTER book_id TYPE UUID');
-        $this->addSql('COMMENT ON COLUMN review.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN review.published_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('COMMENT ON COLUMN review.user_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN review.book_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('ALTER TABLE "user" ALTER id TYPE UUID');
-        $this->addSql('COMMENT ON COLUMN "user".id IS \'(DC2Type:uuid)\'');
-        $this->addSql('ALTER TABLE bookmark ALTER id TYPE UUID');
-        $this->addSql('ALTER TABLE bookmark ALTER bookmarked_at TYPE TIMESTAMP(0) WITHOUT TIME ZONE');
-        $this->addSql('ALTER TABLE bookmark ALTER user_id TYPE UUID');
-        $this->addSql('ALTER TABLE bookmark ALTER book_id TYPE UUID');
-        $this->addSql('COMMENT ON COLUMN bookmark.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN bookmark.bookmarked_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('COMMENT ON COLUMN bookmark.user_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN bookmark.book_id IS \'(DC2Type:uuid)\'');
-
         // Remove isPromoted column
         $this->addSql('ALTER TABLE book DROP is_promoted');
     }
